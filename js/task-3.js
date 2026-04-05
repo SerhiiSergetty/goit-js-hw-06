c18d870d8c5e90926e0d79cb76628b9d30d222ab
Serhii Sergetty <serhiitashkinov@gmail.com>
2026-04-05 20:35:50 +0200

    task123

---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value; //повертає поточне значення приватної властивості value
+  }
+  padEnd(str) {
+    this.#value = this.#value + str; //отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+  }
+  padStart(str) {
+    this.#value = str + this.#value; // отримує параметр str (рядок) і додає його на початок і в кінець
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
